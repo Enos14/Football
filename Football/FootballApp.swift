05aa835e0911fdd459922ecf664a8874052ae681
Enos14 <scottrightpro@gmail.com>
2025-01-15 18:10:46 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Scott Jorgensen on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
